d9004682c4661a98e7d1413e7c1a413efee6fe45
Prabhath <prabathherath007@gmail.com>
2021-10-05 17:19:59 +0530

    first commit

--- new2211/src/com/company/Main.java
+++ new2211/src/com/company/Main.java
@@ -2,7 +2,34 @@ package com.company;
 
 public class Main {
 
+    private static System system;
+
+
+    static String name= "Prabhath";
+    public static final int serialNum=10;
+    //boolean isActive = false;
+
+    //
     public static void main(String[] args) {
 	// write your code here
+        system.out.println ("Hello world!");
+        system.out.println("Name: "+ name);
+
+        //serialNum = 18;
+        //name= "Herath";
+
+        String myName= "Prabhath" + " " + "Herath";
+        system.out.println(myName);
+
+        int valA =10;
+        double valB = (double) valA;
+
+       //specific reason
+        /*
+        int valC = valA / (int) 2.5;
+        system.out.println("valC = " + valC);
+        */
+
+
     }
 }
